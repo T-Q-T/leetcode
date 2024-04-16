a7b50594abcbb2f8a96a353038338376b6230187
qintian <2621159528@qq.com>
2024-04-16 23:29:57 +0800

    feat: 189

--- 189. 轮转数组/index.js
+++ 189. 轮转数组/index.js
@@ -4,19 +4,21 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-    let res = [],
-      count = 0,
-      i = nums.length - (k % nums.length)
-    while (count < nums.length) {
-      res.push(nums[i]);
-      i++;
-      count++;
-      if (i === nums.length ) {
-        i =0;
-      }
+  let res = [],
+    count = 0,
+    i = nums.length - (k % nums.length);
+  while (count < nums.length&&i<nums.length-1) {
+    res.push(nums[i]);
+    i++;
+    count++;
+    if (i === nums.length) {
+      i = 0;
     }
-    return res;
-  };
+  }
+  res.forEach((item, index) => {
+    nums[index] = item;
+  });
+};
 
-let res = rotate([1,2,3,4,5,6,7], 3);
+let res = rotate([1], 1);
 debugger;
